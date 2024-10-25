c33ff0cbd2fa346e24faf197ade09bb21d017528
anishabahl03 <101065093+anishabahl03@users.noreply.github.com>
2024-10-25 14:16:13 -0400

    Lab 6.html

---
+++ Lab 6.js
@@ -0,0 +1,39 @@
+
+var map = L.map('map').setView([39.50, -98.35], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+function getRandomInRange(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    }
+
+const coordinates = []
+ for (let i = 0; i < 3; i++) {
+    const lat = getRandomInRange(30,35,3);
+    const lon = getRandomInRange(-90,-100,3);
+    coordinates.push({lat, lon});
+ }
+
+async function getMarker(){
+    for(let i = 0; i < coordinates.length; i++) {
+        const {lat: latitude, lon: longitude} = coordinates[i];
+
+        await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en')
+            .then(response => response.json())
+            .then(data => {
+                const marker = L.marker([latitude, longitude]).addTo(map);
+                document.getElementById("marker1").innerHTML += ` Latitude: ${latitude}, Longtitude: ${longitude}
+                <br> Locality: ${data.locality}<br>`
+                document.getElementById("marker2").innerHTML += ` Latitude: ${latitude}, Longtitude: ${longitude}
+                <br> Locality: ${data.locality}<br>`
+                document.getElementById("marker3").innerHTML += ` Latitude: ${latitude}, Longtitude: ${longitude}
+                <br> Locality: ${data.locality}<br>`
+
+            });
+    }
+    map.fitBounds(coordinates.map(coord => [coord.lat, coord.lon]));
+}
+window.onload = getMarker;
