bab0563f9af5ce5ba6c42b718ee1782bf351acc7
anishabahl21 <anisha19bahl@gmail.com>
2024-10-27 22:11:10 -0400

    Add files via upload

--- Lab 6.js
+++ Lab 6.js
@@ -1,14 +1,14 @@
 
-var map = L.map('map').setView([39.50, -98.35], 13);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-function getRandomInRange(from, to, fixed) {
-    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-    }
+        function getRandomInRange(from, to, fixed) {
+            return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+            }
 
 const coordinates = []
  for (let i = 0; i < 3; i++) {
@@ -20,7 +20,7 @@ const coordinates = []
 async function getMarker(){
     for(let x = 0; x < coordinates.length; x++) {
         const {lat: latitude, lon: longitude} = coordinates[x];
-        await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en')
+        await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
             .then(response => response.json())
             .then(data => {
                 const marker = L.marker([latitude, longitude]).addTo(map);
